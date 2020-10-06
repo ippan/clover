da55b1e04e7fee3788b64081b2bca8205c3f8fb8
PanPan <ippan829@gmail.com>
2020-10-07 00:03:04 +0800

    add a script examples folder

--- crates/clover-cli/src/main.rs
+++ crates/clover-cli/src/main.rs
@@ -3,11 +3,24 @@ use std::collections::HashMap;
 use clover::compiler::Compiler;
 use clover::runtime::assembly::Assembly;
 use clover::runtime::state::State;
+use std::fs::read_to_string;
+use std::env;
 
 fn main() {
+    let arg: Vec<String> = env::args().collect();
+
+    if arg.len() < 2 {
+        println!("usage:\n\t./clover-cli [file]");
+        return;
+    };
+
+    let filename = arg.get(1).unwrap();
+
+    let source = read_to_string(filename).unwrap();
+
     let mut parser = Parser::new();
 
-    let program_result = parser.parse("local a = 1\nlocal b\nlocal c = true\nreturn 10.2".to_string(), "main".to_string());
+    let program_result = parser.parse(source, "main".to_string());
 
     match program_result {
         Ok(program) => {
